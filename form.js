a4c1b4a9d4f6739a486eb66ebdcea5fe1d051d5f
The-Octab <146798671+The-Octab@users.noreply.github.com>
2023-10-06 17:16:11 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,9 @@
+const nameInput = document.querySelector('input[name="name"]')
+console.log(nameInput);
+
+const emailInput = document.querySelector('input[name="email"]')
+console.log(emailInput);
+
+const PhonenumberInput = document.querySelector('input[name="phonenumber"]')
+console.log(phonenumberInput);
+
